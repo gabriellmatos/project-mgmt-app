8cb410c7b92cc0f48ec773ab1fb5c7d2123eb080
Gabriel Matos <141865582+gabriellmatos@users.noreply.github.com>
2026-01-27 19:32:42 -0300

    deleted some files

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,5 @@
-const { projects, clients } = require('../sampleData.js');
+const Project = require('../models/Project.js');
+const Client = require('../models/Client.js');
 
 const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList } = require('graphql');
 
@@ -24,7 +25,7 @@ const ProjectType = new GraphQLObjectType({
 		client: {
 			type: ClientType,
 			resolve(parent, args) {
-				return clients.find((client) => client.id === parent.clientId);
+				return Client.findById(parent.id);
 			},
 		},
 	}),
@@ -36,27 +37,27 @@ const RootQuery = new GraphQLObjectType({
 		clients: {
 			type: new GraphQLList(ClientType),
 			resolve(parent, args) {
-				return clients;
+				return Client.find();
 			},
 		},
 		client: {
 			type: ClientType,
 			args: { id: { type: GraphQLID } },
 			resolve(parent, args) {
-				return clients.find((client) => client.id === args.id);
+				return Client.findById(args.id);
 			},
 		},
 		projects: {
 			type: new GraphQLList(ProjectType),
 			resolve(parent, args) {
-				return projects;
+				return Project.find();
 			},
 		},
 		project: {
 			type: ProjectType,
 			args: { id: { type: GraphQLID } },
 			resolve(parent, args) {
-				return projects.find((project) => project.id === args.id);
+				return Project.findById(args.id);
 			},
 		},
 	},
