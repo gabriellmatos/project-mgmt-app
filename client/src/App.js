11a7a636ac47bb75446ad4858d341b2624185e9f
Gabriel Matos <141865582+gabriellmatos@users.noreply.github.com>
2026-01-31 19:23:46 -0300

    setting up apollo client v4

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
+import Header from './components/Header';
+import Clients from './components/Clients';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
+
+const client = new ApolloClient({
+	link: new HttpLink({ uri: 'http://localhost:5000/graphql' }),
+	cache: new InMemoryCache(),
+});
+
 function App() {
 	return (
-		<div className='container'>
-			<h1>Hello World</h1>
-		</div>
+		<ApolloProvider client={client}>
+			<Header />
+			<div className='container'>
+				<Clients />
+			</div>
+		</ApolloProvider>
 	);
 }
 
